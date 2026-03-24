4a2a1a2a45059311d73e5ec5f3b6610a64d14ba4
Amey Thakur <ameythakur20@gmail.com>
2026-03-24 17:09:28 -0400

    Kaggle Competitions

--- tmp-utility-official/my_utility.py
+++
@@ -1,6 +0,0 @@
-def hello_utility():
-    print("Utility Function Executed!")
-
-def get_timestamp():
-    import time
-    return time.ctime()
--- tmp-utility-user/user_script.py
+++
@@ -1,7 +0,0 @@
-import os
-import performance_util_badge
-
-# Using the utility script we just created to earn the Utility Scripter badge!
-print("Imported utility script successfully.")
-performance_util_badge.print_memory_usage("Utility Test")
-performance_util_badge.seed_everything(1234)
